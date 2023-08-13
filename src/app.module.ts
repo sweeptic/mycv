4dc78dad5634e7eb394a392fbfd472342f9f68ab
Attila Horvath <sweeptic@gmail.com>
2023-08-13 11:27:52 +0200

    persisting data with typeORM section 41-49

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/reports.entity';
+
 
 @Module({
-  imports: [UsersModule, ReportsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports:
+        [
+            TypeOrmModule.forRoot({
+                type: 'sqlite',
+                database: 'db.sqlite',
+                entities: [User, Report],
+                synchronize: true
+            }),
+            UsersModule, ReportsModule
+        ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsEmail } from "class-validator"
+
+
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string
+
+    @IsString()
+    password: string
+}
